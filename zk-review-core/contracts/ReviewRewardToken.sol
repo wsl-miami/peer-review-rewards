05f6f8059b6d2e540619a5735546f99fcac06b36
Pratiksha Shrestha <pratiksha.shrestha97@gmail.com>
2024-01-30 21:18:03 -0500

    Base token economics setup for transferable review reward token (RRT)

---
+++ zk-review-core/contracts/ReviewRewardToken.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+
+contract ReviewRewardToken is ERC20, ERC20Burnable {
+    address payable public owner;
+    uint256 public blockReward;
+
+    constructor(uint256 reward) ERC20("OceanToken", "RRT") {
+        owner = payable(msg.sender);
+        _mint(owner, 5000 * (10 ** decimals()));
+        blockReward = reward * (10 ** decimals());
+    }
+
+    function _mintMinerReward() internal {
+        _mint(block.coinbase, blockReward);
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256 value) internal virtual override {
+        // if(from != address(0) && to != block.coinbase && block.coinbase != address(0)) {
+        //     _mintMinerReward();
+        // }
+        super._beforeTokenTransfer(from, to, value);
+    }
+
+    function setBlockReward(uint256 reward) public onlyOwner {
+        blockReward = reward * (10 ** decimals());
+    }
+
+    function destroy() public onlyOwner {
+        selfdestruct(owner);
+    }
+
+    modifier onlyOwner {
+        require(msg.sender == owner, "Only the owner can call this function");
+        _;
+    }
+}
