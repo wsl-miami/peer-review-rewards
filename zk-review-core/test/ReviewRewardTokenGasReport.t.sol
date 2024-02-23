8d60b17c18d134909eb3701abb52242c6262d1fd
Pratiksha Shrestha <pratiksha.shrestha97@gmail.com>
2024-02-22 20:32:54 -0500

    Add Soulbound token contract for gas reports

--- zk-review-core/test/ReviewRewardTokenGasReport.t.sol
+++ zk-review-core/test/ReviewRewardTokenGasReport.t.sol
@@ -5,8 +5,10 @@ import "forge-std/Test.sol";
 import "forge-std/Vm.sol";
 
 import "../contracts/ReviewRewardToken.sol";
+import "../contracts/SoulBoundToken.sol";
 
-contract ReviewRewardTokenTest is Test {
+
+contract TokenTest is Test {
     ReviewRewardToken public rrt;
 
     address owner;
@@ -16,24 +18,33 @@ contract ReviewRewardTokenTest is Test {
     uint256 amount;
     uint256 reward;
 
+    Soulbound public sbt;
+    address journal;
+
     function setUp() public {
         reward = 5;
         owner = vm.addr(1);
         vm.startPrank(owner);
         rrt = new ReviewRewardToken(reward);
+        sbt = new Soulbound();
         vm.stopPrank();
 
         admin = vm.addr(2);
-        reviewer1 = vm.addr(3);
-        reviewers.push(vm.addr(4));
+        journal = vm.addr(3);
+        reviewer1 = vm.addr(4);
         reviewers.push(vm.addr(5));
         reviewers.push(vm.addr(6));
+        reviewers.push(vm.addr(7));
+
+        // for (uint256 i = 0; i < 27; i++) {
+        //     reviewers.push(vm.addr(i+8));
+        // }
 
         amount = 3;
     }
 
-    function test_RRTGasTimes() public {
-        for (uint256 i = 0; i < 20; i++) {
+    function test_GasTimes() public {
+        for (uint256 i = 0; i < 500; i++) {
             vm.startPrank(owner);
             rrt.addAdmin(admin);
             rrt.revokeAdmin(admin);
@@ -41,10 +52,58 @@ contract ReviewRewardTokenTest is Test {
         }
 
         vm.startPrank(owner);
-        for (uint256 i=0; i<100; i++) {
+        for (uint256 i=0; i<500; i++) {
             rrt.bulkMint(reviewers, amount);
             rrt.individualMint(reviewer1, amount);
+            rrt.balanceOf(reviewer1);
+
+            sbt.bulkMintFromCron(reviewers, vm.toString(journal));
+            sbt.safeMint(reviewer1, vm.toString(journal));
+            sbt.getTokensOwnedByAddress(reviewer1);
+            sbt.tokenURI(1);
         }
         vm.stopPrank();
     }
 }
+
+
+// contract SoulboundTest is Test {
+
+//     address owner;
+//     address admin;
+//     address reviewer1;
+//     address[] reviewers;
+
+//     Soulbound public sbt;
+//     address journal;
+
+//     function setUp() public {
+//         owner = vm.addr(1);
+//         vm.startPrank(owner);
+//         sbt = new Soulbound();
+//         vm.stopPrank();
+
+//         admin = vm.addr(2);
+//         journal = vm.addr(3);
+//         reviewer1 = vm.addr(4);
+//         reviewers.push(vm.addr(5));
+//         reviewers.push(vm.addr(6));
+//         reviewers.push(vm.addr(7));
+
+//         for (uint256 i = 0; i < 7; i++) {
+//             reviewers.push(vm.addr(i+8));
+//         }
+//     }
+
+//     function test_SBTGasTimes() public {
+
+//         vm.startPrank(owner);
+//         // for (uint256 i=0; i<100; i++) {
+//             sbt.bulkMintFromCron(reviewers, vm.toString(journal));
+//             sbt.safeMint(reviewer1, vm.toString(journal));
+//             sbt.getTokensOwnedByAddress(reviewer1);
+//             sbt.tokenURI(1);
+//         // }
+//         vm.stopPrank();
+//     }
+// }
